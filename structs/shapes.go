1078498c96178feada6fc313ec619950552f02da
David Pierre <davidpierrealves21@gmail.com>
2020-07-12 03:02:38 -0300

    Add code from the structures methods and interface section

---
+++ structs/shapes.go
@@ -0,0 +1,40 @@
+package structs
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+// Perimeter receives width and height and returns perimeter
+func (r Rectangle) Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+// Area receives width and height and returns area
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+// Area receives width and height and returns area
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.Radius, 2)
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
+}
