7b79a61db55853cf58bff6553a7a4c61728442a6
David Pierre <davidpierrealves21@gmail.com>
2020-07-19 04:42:41 -0300

    Add final code for the mocking chapter

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -7,12 +7,6 @@ import (
 	"time"
 )
 
-type DefaultSleeper struct{}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
-
 type Sleeper interface {
 	Sleep()
 }
@@ -28,7 +22,16 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 	fmt.Fprint(out, finalWord)
 }
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
