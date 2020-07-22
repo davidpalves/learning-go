70d24ae8893dba3487b3ab5775b2d7583bb07016
David Pierre <davidpierrealves21@gmail.com>
2020-07-22 00:59:12 -0300

    Add select chapter code

---
+++ select/racer.go
@@ -0,0 +1,36 @@
+package racer
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var timeout = 10 * time.Second
+
+// Racer is a function that wraps ConfigurableRacer using the default timeout (10 seconds)
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, timeout)
+}
+
+// ConfigurableRacer is a function that returns the fastest response beetween two URLs
+// and has a timeout of 10 seconds
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
