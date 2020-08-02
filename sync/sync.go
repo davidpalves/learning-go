97f88af75ebb16bdb9d1a66ecde6397eaecf152a
David Pierre <davidpierrealves21@gmail.com>
2020-08-01 23:23:29 -0300

    Add Sync chapter code

--- sync/sync.go
+++ sync/sync.go
@@ -2,15 +2,18 @@ package counter
 
 import "sync"
 
+// Counter is a struct that takes a value and a mutex
 type Counter struct {
 	mu    sync.Mutex
 	value int
 }
 
+// NewCounter is a wrapper of counter
 func NewCounter() *Counter {
 	return &Counter{}
 }
 
+// Inc is a method to imcrement the counter
 func (c *Counter) Inc() {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -18,6 +21,7 @@ func (c *Counter) Inc() {
 	c.value++
 }
 
+// Value is a method to return the counter value
 func (c *Counter) Value() int {
 	return c.value
 }
